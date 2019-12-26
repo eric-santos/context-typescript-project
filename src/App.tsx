3999ce7f1f85812451801671321c1acb9643ce95
eric <Eric355402@yahoo.com>
2019-12-26 14:23:18 -0700

    completed basic app with flex and sticky navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,67 @@
 import React from 'react';
 
-import './App.css';
+import { Store } from './Store';
 
-function App() {
+interface IEpisode {
+  airdate: string;
+  airstamp: string;
+  airtime: string;
+  id: number;
+  image: { medium: string; original: string };
+  name: string;
+  number: number;
+  runtime: number;
+  season: number;
+  summary: string;
+  url: string;
+}
+
+function App(): JSX.Element {
+  const { state, dispatch } = React.useContext(Store);
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
+
+  const fetchDataAction = async () => {
+    const URL =
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
+    const data = await fetch(URL);
+    const dataJSON = await data.json();
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Test Commit</h1>
+    <React.Fragment>
+      <header className="header">
+        <h1>Rick and Morty</h1>
+        <p>Pick your favorite episode</p>
       </header>
-    </div>
+      <section className="episode-layout">
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id}>
+              <img
+                className="episode-box"
+                src={episode.image.medium}
+                alt={`Rick and Morty ${episode.name}`}
+              />
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season} Episode: {episode.number}
+              </section>
+            </section>
+          );
+        })}
+      </section>
+    </React.Fragment>
   );
 }
 
 export default App;
+
+//api
+//https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes
--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { StoreProvider } from './Store';
+
+ReactDOM.render(
+  <StoreProvider>
+    <App />
+  </StoreProvider>,
+  document.getElementById('root')
+);
